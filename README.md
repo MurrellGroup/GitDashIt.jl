08305b96753b8de0ba9d2cc1827bbc3a1e123dfd
Ben Murrell <murrellb@gmail.com>
2023-05-26 01:07:01 +0200

    First and Last

--- README.md
+++ README.md
@@ -1,6 +1,51 @@
 # GitDashIt
 
+GitHub is a free dashboard you can push plots to.
+
 [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://murrellb.github.io/GitDashIt.jl/stable/)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://murrellb.github.io/GitDashIt.jl/dev/)
 [![Build Status](https://github.com/murrellb/GitDashIt.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/murrellb/GitDashIt.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/murrellb/GitDashIt.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/murrellb/GitDashIt.jl)
+
+### Example.
+First, create a GitHub repo that will host your GitDashIt dashboard. Then clone the repo somewhere you'll be generating the plots. Make a file called `GitDashItSafe` at the base of the repo, to let GitDashIt know that it is safe. From within that repo, run something like this:
+"""
+```julia-repl
+#Initialize
+gd = GitDash()
+
+#Check saftey
+gitdash_issafe(gd)
+
+#If you didn't make the file, then override saftey
+GitDashIt.gd_force_allow(gd)
+gitdash_issafe(gd)
+
+#Add a dash of text
+dashprintln(gd, "### Plots")
+dashprintln(gd, "Some text")
+
+#And some figures
+pl = plot(rand(100))
+savefig(pl,dashfile(gd,"plot1.svg"))
+
+dashprintln(gd,"### Another Heading!")
+dashprintln(gd,"Another plot:")
+pl = plot(sort(rand(100)))
+savefig(pl,dashfile(gd,"plot2.svg"))
+
+#Write to the local README.md, overwriting whatever was in there
+flush2local(gd)
+
+#Add more figures
+dashprintln(gd,"### A Third Heading!")
+pl = plot(sort(rand(100),rev=true))
+savefig(pl,dashfile(gd,"plot3.svg"))
+
+#Append to the local readme
+flush2local(gd, append = true)
+
+#Push to the remote repo, which is now your dashboard!
+pushdash(gd)
+```
+"""
--- src/GitDashIt.jl
+++ src/GitDashIt.jl
@@ -1,5 +1,99 @@
+#Should we allow a branch to be specified?
+#Currently using system git, because unclear how to handle credentials on Julia git
+#Need to test when running this not from withing the base of the repo
+#likely a few joinpath statements missing
+
 module GitDashIt
 
-# Write your package code here.
+export GitDash, dashfile, pushdash, flush2local, dashprint, dashprintln, gitdash_issafe
+
+function git()
+    return "git"
+end
+
+mutable struct GitDash
+    text::String
+    plotfiles::Vector{String}
+    basepath::String
+    function GitDash(; basepath = ".")
+        return new("", [],basepath)
+    end
+end
+
+function dashprint(gd::GitDash,txt)
+    gd.text *= txt
+    nothing
+end
+
+function dashprintln(gd::GitDash,txt)
+    gd.text *= txt * "\n"
+    nothing
+end
+
+"""
+    dashfile(gd::GitDash,filepath::String; width = 2000)
+
+Injects an image link into the README.md file. The image is saved in the current directory.
+
+# Example
+```julia-repl
+    Plots.savefig(pl, dashfile(gd,"plot.svg"))
+```
+"""
+function dashfile(gd::GitDash,filepath::String; width = 2000)
+    if !gitdash_issafe(gd)
+        unsafe_error(gd)
+    end
+    push!(gd.plotfiles,filepath)
+    gd.text *= "<img src=\"" * filepath * "\" style=\"width: $(width)px;\"> \n\n"
+    return filepath
+end
+
+function unsafe_error(gd)
+    @error "GitDashIt is not running in repo known to be safe. Currently running from $(pwd()) with a base path of $(gd.basepath)
+    Add a file named `GitDashItSafe` to the base of the repo, or, if you are ABSOLUTELY sure you're in the right place, run `GitDashIt.gd_force_allow(gd)` to add this file for you."
+end
+
+function gitdash_issafe(gd::GitDash)
+    return isfile(joinpath(gd.basepath,"GitDashItSafe"))
+end
+
+function gd_force_allow(gd::GitDash)
+    touch(joinpath(gd.basepath,"GitDashItSafe"))
+    run(`$(git()) -C $(gd.basepath) add GitDashItSafe`);
+    nothing
+end
+
+function flush2local(gd::GitDash; filepath = "README.md", append = false)
+    if !gitdash_issafe(gd)
+        unsafe_error(gd)
+    end
+    f = open(filepath,append ? "a" : "w")
+                    write(f, gd.text)
+                    close(f)
+    for p in gd.plotfiles
+        run(`$(git()) -C $(gd.basepath) add $(p)`);
+    end
+    run(`$(git()) -C $(gd.basepath) add README.md`);
+    gd.text = ""
+    gd.plotfiles = []
+    nothing
+end
+
+#=
+function clonedash(gd::GitDash,repo::String)
+    run(`$(git()) -C $(gd.basepath) clone $(repo)`)
+end
+=#
+
+function pushdash(gd::GitDash; message = "Updating...")
+    if !gitdash_issafe(gd)
+        unsafe_error(gd)
+    end
+    run(`$(git()) -C $(gd.basepath) commit -m "$(message)"`);
+    run(`$(git()) -C $(gd.basepath) push`);
+    nothing
+end
+
 
 end
